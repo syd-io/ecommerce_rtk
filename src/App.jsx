13b5bb9f2d0569d62d075c3d52efe7ee16d23236
syd-io <sy.isahd@gmail.com>
2025-02-13 16:28:50 -0500

    implemented supercoin

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@
 import React from 'react';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
+import SuperCoin from './Components/SuperCoin';
 import './App.css'
 const App = () => {
-  return (
- 
-      <div>
-        <h1 className='app-heading'>E-Commerce Application</h1>
-        <ProductList />
-        <ShoppingCart />
-      </div>
+    return (
 
-  );
+        <div>
+            <h1 className='app-heading'>E-Commerce Application</h1>
+            <ProductList />
+            <ShoppingCart />
+            <SuperCoin />
+        </div>
+
+    );
 };
 
 export default App;
